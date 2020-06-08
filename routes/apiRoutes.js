2e5a3bbd91ac241323e76443c5a53e19de2d7f4a
Shannon Trainor <sltrainor@outlook.com>
2020-06-08 19:39:58 -0400

    added comments for routing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
     //require express & require router function (creates router)
 
-    // add routes
+    //**ADD ROUTES**
+    //TODO: route for saving activity data
+
+    //TODO: route for getting activity data by duration
+
+    //TODO: route for getting activity data by milage
 
     
 
