005e6dd65f00eae0618985b431df5b0fbe35069d
Shannon Trainor <sltrainor@outlook.com>
2020-06-08 20:09:36 -0400

    added html home route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
-    //require express & require router function (creates router)
+const path = require("path");
+
+//require express & require router function (creates router)
 
     // add routes
 
+    //HTML routes for displaying homepage
+    router.get("/", (req, res) => {
+        const filePath = path.join(__dirname, "..", "public", "html", "index.html");   //join folders/files together - dirname = path to current file
+        res.sendFile(filePath);
+    });
 
 
 
@@ -22,6 +29,6 @@ const router = require("express").Router();
 
 
 
-    
+
   // export router created    
   module.exports = router;
